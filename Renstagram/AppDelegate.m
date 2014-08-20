031b5bbd4bc88b3bb3eda66d4ce75e219688a1fa
horngep <horng.ep@hotmail.com>
2014-08-20 11:53:03 -0500

    greener

--- Renstagram/AppDelegate.m
+++ Renstagram/AppDelegate.m
@@ -22,9 +22,7 @@
       [UIColor whiteColor],NSForegroundColorAttributeName,
       [UIFont fontWithName:@"Futura" size:22.0], NSFontAttributeName,nil]];
 
-    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
-
-
+    [[UINavigationBar appearance] setTintColor:[UIColor greenColor]];
 
     return YES;
 }
